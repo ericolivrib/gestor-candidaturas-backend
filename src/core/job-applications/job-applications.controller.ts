227a59dad861ef23d6633963c6501e68ed1f6349
Érico <oliv.ericorib@gmail.com>
2025-08-25 19:08:25 -0300

    feat(job-appplications): criar vaga de usuário (somente `jobTitle`)

--- src/core/job-applications/job-applications.controller.ts
+++ src/core/job-applications/job-applications.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Logger, Post } from '@nestjs/common';
 import { JobApplicationsService } from './job-applications.service';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { AuthUserDto } from 'src/shared/dto/auth-user.dto';
 
-@Controller('job-applications')
+@Controller('v1/job-applications')
 export class JobApplicationsController {
-  constructor(private readonly jobApplicationsService: JobApplicationsService) {}
+  private readonly logger: Logger = new Logger(JobApplicationsController.name);
+
+  constructor(private readonly jobApplicationsService: JobApplicationsService) { }
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  async createJobApplication(@CurrentUser() user: AuthUserDto, @Body() jobApplicationDto: object) {
+    this.logger.log('Criando aplicação de vaga');
+    await this.jobApplicationsService.createJobApplication(user.id, jobApplicationDto);
+  }
 }
--- src/core/job-applications/job-applications.service.ts
+++ src/core/job-applications/job-applications.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class JobApplicationsService {}
+export class JobApplicationsService {
+  constructor(
+    private readonly prismaService: PrismaService
+  ) { }
+
+  async createJobApplication(userId: string, createJobApplicationDto: any) {
+    await this.prismaService.jobApplication.create({
+      data: {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        jobTitle: <string>createJobApplicationDto['jobTitle'],
+        status: 'DRAFT',
+        userId
+      }
+    });
+  }
+}
